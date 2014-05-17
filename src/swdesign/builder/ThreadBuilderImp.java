1485f1d4df18db4dfe928d742997ed26e8552e56
Adam Vongrej <vongrejadam@gmail.com>
2014-05-17 11:10:02 +0200

    Sort added

--- src/swdesign/builder/ThreadBuilderImp.java
+++ src/swdesign/builder/ThreadBuilderImp.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package swdesign.builder;
 
 import com.sun.javafx.geom.Crossings;
+import java.util.Arrays;
 import swdesign.game.Game;
 import swdesign.game.GameInstance.Result;
 import swdesign.gameListeners.Constants;
@@ -19,40 +19,41 @@ import swdesign.tournament.Tournament;
  *
  * @author adamv_000
  */
-public class ThreadBuilderImp implements IThreadBuilder{
+public class ThreadBuilderImp implements IThreadBuilder {
 
     @Override
     public Thread buildThread(Tournament tournament) {
-        return new Thread(){
+        return new Thread() {
 
             @Override
             public void run() {
-                
+
                 MatchInfo match;
                 Game game = tournament.getGame();
                 Result result;
-                
-                while((match = tournament.getNextMatch()) != null){
-                    result = game.newInstance().playGame(((ParticipantImp)match.getParticipantA()).newInstance(), ((ParticipantImp)match.getParticipantB()).newInstance());
+
+                while ((match = tournament.getNextMatch()) != null) {
+                    result = game.newInstance().playGame(((ParticipantImp) match.getParticipantA()).newInstance(), ((ParticipantImp) match.getParticipantB()).newInstance());
                     tournament.notifyListeners(new TournamentEvent(Constants.MATCH_START.replace("{name1}", match.getParticipantA().getName())
                             .replace("{name2}", match.getParticipantB().getName()), TournamentEvent.TYPE_MATCH_STARTED, tournament));
-                    if (result == Result.AWINS){
+                    if (result == Result.AWINS) {
                         match.getParticipantA().updateScore(1);
-                        match.getParticipantB().updateScore(-1); 
-                    }
-                    else if (result == Result.BWINS){
+                        match.getParticipantB().updateScore(-1);
+                    } else if (result == Result.BWINS) {
                         match.getParticipantA().updateScore(-1);
-                        match.getParticipantB().updateScore(1); 
+                        match.getParticipantB().updateScore(1);
                     }
                     tournament.notifyListeners(new TournamentEvent(Constants.MATCH_END.replace("{name1}", match.getParticipantA().getName())
-                                .replace("{name2}", match.getParticipantB().getName()).replace("{matchResult}", result.toString()), TournamentEvent.TYPE_MATCH_STARTED, tournament));
+                            .replace("{name2}", match.getParticipantB().getName()).replace("{matchResult}", result.toString()), TournamentEvent.TYPE_MATCH_STARTED, tournament));
                     match.setResult(result);
-                    if (!tournament.areOtherThreadsAlive(this)){
-                        tournament.notifyListeners(new TournamentEvent(tournament.getParticipants(), TournamentEvent.TYPE_TOURNAMENT_ENDED, tournament));
-                    }
+
+                }
+                if (!tournament.areOtherThreadsAlive(this)) {
+                    Arrays.sort(tournament.getParticipants());
+                    tournament.notifyListeners(new TournamentEvent(tournament.getParticipants(), TournamentEvent.TYPE_TOURNAMENT_ENDED, tournament));
                 }
             }
         };
     }
-    
+
 }
